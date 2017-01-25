5aafaee9b00e2b495557d9892ec7cca6b720889d
Jon Moss <me@jonathanmoss.me>
2017-01-25 15:47:12 -0500

    Setup socket.io + have basic standby messaging

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-var express = require('express');
-var app = express();
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000, function() {
-  console.log('Lightswitch is now listening on port 3000...');
+io.on('connection', function(socket) {
+  console.log('a user connected');
+
+  socket.on('standby', function(msg) {
+    console.log('broadcasting standby...');
+    io.emit('standby', msg);
+  });
+
+  socket.on('disconnect', function() {
+    console.log('a user disconnected');
+  });
+});
+
+http.listen(3000, function() {
+  console.log('listening on *:3000');
 });
